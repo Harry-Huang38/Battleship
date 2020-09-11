0c458cead5998d56008695833b9797f779c413e4
Maokun Huang <huangmaokun0308@gmail.com>
2020-09-10 19:00:23 -0500

    add check winner

--- grid.py
+++ grid.py
@@ -25,3 +25,47 @@ class gridWrapper:
                 #print("You Missed")
 
     #def checkWinner(self)
+    def __winner__ (self,x):
+        p1_count = 0
+        p2_count = 0
+        win_count = x
+        for i in range(1,10):
+            for j in range(1,10):
+                if self.grid[j][i]=="hit":
+                    p1_count += 1
+        for p in range(11,20):
+            for q in range(0,10):
+                if self.grid[q][p]=="hit":
+                    p2_count += 1
+        
+        if p1_count==5 or p1_count==4 or p1_count==2:
+            p1_count=0
+
+        if p1_count==3:
+            p1_count=2
+        if p1_count==6:
+            p1_count=3
+        if p1_count==10:
+            p1_count=4
+        if p1_count==15:
+            p1_count=5  
+            
+        if p2_count==5 or p2_count==4 or p2_count==2:
+            p2_count=0
+           
+        if p2_count==3:
+            p2_count=2
+        if p2_count==6:
+            p2_count=3
+        if p2_count==10:
+            p2_count=4
+        if p2_count==15:
+            p2_count=5
+
+        if p1_count == win_count:
+            print("Congratulations!! Player 1 won")
+            return True
+        
+        if p2_count == win_count:
+           print("Congratulations!! Player 2 won")
+           return True
